477d450319dc42def75ea0a13e0834263d6aaa35
Michelle Steigerwalt <msteigerwalt@gmail.com>
2018-02-07 15:15:00 -0500

    Docs and reordering for readability.

--- chunktheplanet.go
+++ chunktheplanet.go
@@ -1,4 +1,28 @@
-package main
+// Works with the Protocol Buffer Binary Format (PBF) of OpenStreetMap 
+// planetfiles to break up a large file into many smaller, standalone files
+// that can be used like their larger counterparts for import and other data
+// pipeline uses.
+//
+// This can greatly speed up planetfile import, as it allows for multiple
+// chunks to be imported through several different threads or on different
+// machines running parallel operations.
+//
+// Additionally, this package provides multiple streaming modes for remote
+// servers which provide HTTP Range support, allowing for data to be 
+// imported concurrently with its transfer.
+//
+// This package only deals with PBFs at the Block level, and does not
+// directly interact with or modify more sophisticated datatypes of the
+// OSMPBF format corresponding to the map data itself, such as
+// PrimitiveGroups, Nodes, Ways, or Relations.
+//
+// If you'd like to interact with the map-related data using Go, you
+// can do so using the OSMPBF library:
+// http://github.com/qedus/osmpbf
+//
+// For more information on the Protocol Buffer Binary Format:
+// https://wiki.openstreetmap.org/wiki/PBF_Format
+package chunktheplanet
 
 import (
 	"log"
@@ -23,128 +47,6 @@ func check(e error) {
 	if e != nil {
 		log.Fatal(e)
 	}
-}
-
-type Block struct {
-	Data []byte
-	DataType string
-	Size int32
-	Blob *OSMPBF.Blob
-	BlobHeader *OSMPBF.BlobHeader
-	SizeBlock []byte
-	StartByte int64
-}
-
-type Stream interface {
-	Close() error
-	Read([]byte) (int, error)
-	Rewind()
-	SetCursor(int64) (error)
-	GetCursor() (int64, error)
-}
-
-type RateLimiter struct {
-	lastRequest time.Time
-	requestRate time.Duration
-}
-
-// File IO
-type IOStream struct {
-	Location string
-	file *os.File
-}
-
-// HTTP Streaming
-type HttpStream struct {
-	Location string
-	cur int64
-	client *http.Client
-	RateLimiter *RateLimiter
-}
-
-type Planetfile struct {
-	Location string
-	stream Stream
-}
-
-func OpenIO(filename string) (*IOStream, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return &IOStream{}, err
-	}
-	return &IOStream{filename, file}, nil
-}
-
-func OpenHttp(url string) (*HttpStream, error) {
-	return &HttpStream{url, 0, &http.Client{}, &RateLimiter{time.Now(), 0*time.Second}}, nil
-}
-
-func OpenStream(loc string) (*Planetfile, error) {
-	if strings.HasPrefix(loc, "http") {
-		s, e := OpenHttp(loc)
-		return &Planetfile{loc, s}, e
-	}
-	s, e := OpenIO(loc)
-	return &Planetfile{loc, s}, e
-}
-
-func (s IOStream) Close() error {
-	return s.file.Close()
-}
-
-func (s IOStream) Read(buff []byte) (int, error) {
-	return s.file.Read(buff)
-}
-
-func (s IOStream) Rewind() {
-	s.SetCursor(0)
-}
-
-func (s IOStream) GetCursor() (int64, error) {
-	return s.file.Seek(0, 1)
-}
-
-func (s IOStream) SetCursor(i int64) (error) {
-	_, e := s.file.Seek(i, 0)
-	return e
-}
-
-func (pbf Planetfile) Close() {
-	pbf.stream.Close()
-}
-
-func (s *HttpStream) Read(buff []byte) (int, error) {
-	s.RateLimiter.limit()
-	end := s.cur + int64(len(buff))
-	req, _ := http.NewRequest("GET", s.Location, nil)
-	req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", s.cur, end))
-	s.SetCursor(end)
-	res, err := s.client.Do(req)
-	if err != nil {
-		return 0, err
-	}
-	// Stacked defers are LIFO: Last In, First Out, so these two lines
-	// will actually run backwards.
-	defer res.Body.Close()
-	defer io.Copy(ioutil.Discard, res.Body)
-	return res.Body.Read(buff)
-}
-
-func (s *HttpStream) Rewind() {
-	s.cur = int64(0)
-}
-
-func (s *HttpStream) GetCursor() (int64, error) {
-	return s.cur, nil
-}
-
-func (s *HttpStream) SetCursor(i int64) (error) {
-	s.cur = i
-	return nil
-}
-
-func (s *HttpStream) Close() (error) {
-	return nil
 }
 
 func main() {
@@ -193,7 +95,178 @@ func main() {
 
 }
 
+// Generic interface for Streams; by default they keep track of their last
+// read position (Cursor) after Reads.
+//
+// Planetfile methods rely on this interface for its streams in order to
+// work properly for various use cases.
+type Stream interface {
+	Close() error
+	Read([]byte) (int, error)
+	Rewind()
+	SetCursor(int64) (error)
+	GetCursor() (int64, error)
+}
+
+// RateLimiter provides a generalized interface for limiting the rate at which
+// requests are made.
+type RateLimiter struct {
+	LastRequest time.Time
+	RequestRate time.Duration
+}
+
+// Sleeps if the last request has happened sooner than the current rate between
+// requests. Once done sleeping, it will automatically update the lastRequest
+// time.
+func (r *RateLimiter) limit() {
+	if r.RequestRate > 0 {
+		start := r.LastRequest
+		now := time.Now()
+		elapsed := now.Sub(start)
+		if elapsed < r.RequestRate {
+			time.Sleep((r.RequestRate-elapsed))
+		}
+	}
+	r.LastRequest = time.Now()
+}
+
+
+// Opens a Stream wrapped in a Planetfile interface based on the protocol
+// specified in the location string.
+func OpenStream(loc string) (*Planetfile, error) {
+	if strings.HasPrefix(loc, "http") {
+		s, e := OpenHttp(loc)
+		return &Planetfile{loc, s}, e
+	}
+	s, e := OpenIO(loc)
+	return &Planetfile{loc, s}, e
+}
+
+// IOStream attaches to a file location.
+type IOStream struct {
+	Location string
+	file *os.File
+}
+
+// Creates a new IOStream and attaches it to the specified file location.
+func OpenIO(filename string) (*IOStream, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return &IOStream{}, err
+	}
+	return &IOStream{filename, file}, nil
+}
+
+// Closes the file attached to this Stream.
+func (s IOStream) Close() error {
+	return s.file.Close()
+}
+
+// Fills the passed in byte array with the appropriate length of content from
+// the current cursor position of the file, incrementing the cursor when
+// complete.
+//
+// The return value is the number of bytes read.
+func (s IOStream) Read(buff []byte) (int, error) {
+	return s.file.Read(buff)
+}
+
+// Sets the current read position to 0.
+func (s IOStream) Rewind() {
+	s.SetCursor(0)
+}
+
+// Returns the current read position.
+func (s IOStream) GetCursor() (int64, error) {
+	return s.file.Seek(0, 1)
+}
+
+// Sets the current read position.
+func (s IOStream) SetCursor(i int64) (error) {
+	_, e := s.file.Seek(i, 0)
+	return e
+}
+
+// HTTP Streaming
+type HttpStream struct {
+	Location string
+	cur int64
+	client *http.Client
+	RateLimiter *RateLimiter
+}
+
+// Opens a URL for streaming.
+//
+// Should work with HTTP as well as HTTPS, but not on servers that don't
+// support byte Range headers.
+func OpenHttp(url string) (*HttpStream, error) {
+	return &HttpStream{url, 0, &http.Client{}, &RateLimiter{time.Now(), 0*time.Second}}, nil
+}
+
+// Makes a partial HTTP request using the Range header for the specified number of bytes
+// from the current read cursor.
+//
+// NB Range requests only work for servers which respond to byte range headers.
+func (s *HttpStream) Read(buff []byte) (int, error) {
+	s.RateLimiter.limit()
+	end := s.cur + int64(len(buff))
+	req, _ := http.NewRequest("GET", s.Location, nil)
+	req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", s.cur, end))
+	s.SetCursor(end)
+	res, err := s.client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	// Stacked defers are LIFO: Last In, First Out, so these two lines
+	// will actually run backwards.
+	defer res.Body.Close()
+	defer io.Copy(ioutil.Discard, res.Body)
+	return res.Body.Read(buff)
+}
+
+func (s *HttpStream) Rewind() {
+	s.cur = int64(0)
+}
+
+func (s *HttpStream) GetCursor() (int64, error) {
+	return s.cur, nil
+}
+
+func (s *HttpStream) SetCursor(i int64) (error) {
+	s.cur = i
+	return nil
+}
+
+// Doesn't actually do anything, as client requests are closed after
+// taking place.
+//
+// Provided to fulfill interface requirements.
+func (s *HttpStream) Close() (error) {
+	return nil
+}
+
+// Planetfile provides high-level interface to the specific streaming
+// functionality.
+type Planetfile struct {
+	Location string
+	stream Stream
+}
+
+// Calls the underlying Stream's Close function. Depending on the type of
+// Stream, this may not have any effect, but it's still a good thing to
+// do.
+func (pbf Planetfile) Close() {
+	pbf.stream.Close()
+}
+
+// Copies and decodes the first block of the Stream, which happens to
+// be a HeaderBlock.
+//
+// You can use this to dump metadata about a file, such as its origin and
+// replication sequence number.
 func (pbf Planetfile) ReadFileHeader() (*OSMPBF.HeaderBlock, error) {
+
+	c := pbf.stream.GetCursor()
 
 	// Start at the beginning 'cause that's where the file header
 	// lives.
@@ -211,6 +284,8 @@ func (pbf Planetfile) ReadFileHeader() (*OSMPBF.HeaderBlock, error) {
 
 	header := new(OSMPBF.HeaderBlock)
 	err = proto.Unmarshal(data, header)
+
+	pbf.stream.SetCursor(c)
 
 	return header, err
 
@@ -270,6 +345,30 @@ func (pbf Planetfile) NextBlock() (*Block, error) {
 
 }
 
+// Blocks are the main file primitive of PBF-based Planetfiles.
+//
+// This package deals with two specific types of Blocks, HeaderBlocks
+// and Blobs.
+//
+// Only one HeaderBlock should exist in the entire file, and it should
+// be the first block in the file. The rest are all Blobs, which
+// themselves contain data which can be subsequently unmarshalled into
+// more specific data types.
+//
+// For the purposes of this package, however, we only deal with the data
+// at the Block level.
+type Block struct {
+	Data []byte
+	DataType string
+	Size int32
+	Blob *OSMPBF.Blob
+	BlobHeader *OSMPBF.BlobHeader
+	SizeBlock []byte
+	StartByte int64
+}
+
+// Dumps the entire Block to the indicated file location, including its
+// leading size delimiter and BlobHeader.
 func (b Block) Write(f *os.File) int {
 	f.Write(b.SizeBlock)
 	data, err := proto.Marshal(b.BlobHeader)
@@ -283,6 +382,12 @@ func (b Block) Write(f *os.File) int {
 	return size
 }
 
+// Decodes the raw data of the Block so that it can be demarshaled.
+//
+// Sometimes the data is gzipped, in which case it will be decompressed
+// before being returned.
+//
+// (Taken directly from qedus/osmpbf.)
 func (b Block) Decode() ([]byte, error) {
 	blob := b.Blob
 	// All of this is taken directly from github.com/qedus/osmpbf/OSMPBF
@@ -309,18 +414,6 @@ func (b Block) Decode() ([]byte, error) {
 	default:
 		return nil, errors.New("unknown blob data")
 	}
-}
-
-func (r *RateLimiter) limit() {
-	if r.requestRate > 0 {
-		start := r.lastRequest
-		now := time.Now()
-		elapsed := now.Sub(start)
-		if elapsed < r.requestRate {
-			time.Sleep((r.requestRate-elapsed))
-		}
-	}
-	r.lastRequest = time.Now()
 }
 
 func humanBytes(s int64) string {
